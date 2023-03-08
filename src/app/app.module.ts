4678244434190197472fa436997020827756e7a4
dsjordao <jordaodaniel@hotmail.com>
2023-03-08 15:25:29 -0300

    working in Progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { GoogleMapsModule } from '@angular/google-maps';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +8,7 @@ import { HeaderComponent } from './core/components/header/header.component';
 import { HomeComponent } from './core/pages/components/home/home.component';
 import { SobreComponent } from './core/pages/components/sobre/sobre.component';
 import { ContatoComponent } from './core/pages/components/contato/contato.component';
+
 
 
 @NgModule({
@@ -21,6 +22,7 @@ import { ContatoComponent } from './core/pages/components/contato/contato.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
+    // GoogleMapsModule
 
   ],
   providers: [],
--- src/app/core/pages/components/contato/contato.component.ts
+++ src/app/core/pages/components/contato/contato.component.ts
@@ -10,5 +10,18 @@ export class ContatoComponent implements OnInit{
 
   constructor() {}
 
-  ngOnInit(): void {}  
+  ngOnInit(): void {}
+  
+  // display: any;
+  //   center: google.maps.LatLngLiteral = {
+  //       lat: 24,
+  //       lng: 12
+  //   };
+  //   zoom = 4;
+  //   moveMap(event: google.maps.MapMouseEvent) {
+  //       if (event.latLng != null) this.center = (event.latLng.toJSON());
+  //   }
+  //   move(event: google.maps.MapMouseEvent) {
+  //       if (event.latLng != null) this.display = event.latLng.toJSON();
+  //   }
 }
